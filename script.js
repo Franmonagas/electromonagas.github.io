b79dc398f8a910e150a8788fc7a4b1c6b5316bd0
Franmonagas <franchescototi83@gmail.com>
2024-10-06 16:44:46 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// script.js
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+        
+        window.scrollTo({
+            top: targetSection.offsetTop - 70, // Ajustar el desplazamiento
+            behavior: 'smooth'
+        });
+    });
+});
